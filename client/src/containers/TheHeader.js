5140390e8815da328c78f1380b02c5ffcc4a3a34
lebaphi <phi@flood.io>
2020-08-05 16:27:24 +0700

    chore: clean code

--- client/src/containers/TheHeader.js
+++ client/src/containers/TheHeader.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import {
 	CHeader,
-	CHeaderBrand,
 	CHeaderNav,
 	CHeaderNavItem,
 	CHeaderNavLink,
@@ -24,10 +23,6 @@ const TheHeader = (props) => {
 
 	return (
 		<CHeader withSubheader>
- 
-			<CHeaderBrand className="mx-auto d-lg-none" to="/">
-				<CIcon name="logo" height="48" alt="Logo"/>
-			</CHeaderBrand>
 
 			<CHeaderNav className="d-md-down-none mr-auto">
 				<CHeaderNavItem className="px-3" >
